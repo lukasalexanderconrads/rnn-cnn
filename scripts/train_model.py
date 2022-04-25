384dbcbaeae25d2f1a6226d9250245fe792f5624
lukasalexanderconrads <Abba1024-github>
2022-04-25 17:41:08 +0200

    added script for training

---
+++ scripts/train_model.py
@@ -0,0 +1,69 @@
+import click
+from pathlib import Path
+import torch
+
+from src.utils import read_yaml, create_instance
+
+
+@click.command()
+@click.option('-c', '--config', 'config_path', required=True, type=click.Path(exists=True))
+
+
+def main(config_path: Path):
+    config = read_yaml(config_path)
+    name = config['name']
+    device = torch.device(config['device'])
+
+    dataset = get_dataset(config['dataset'], device=device)
+
+    model = get_model(config['model'], device=device, in_dim=dataset.data_dim, out_dim=dataset.n_classes)
+
+    loader = get_data_loader(config['loader'], dataset)
+
+    optimizer = torch.optim.Adam(lr=.001, params=model.parameters())
+
+    trainer = get_trainer(config['trainer'], name, model, loader, optimizer)
+
+    trainer.train()
+
+
+def get_trainer(config, name, model, loader, optimizer):
+    module_name = config['module']
+    class_name = config['name']
+    args = config['args']
+    trainer = create_instance(module_name, class_name, args, name, model, loader, optimizer)
+    return trainer
+
+
+def get_model(config, device, in_dim, out_dim):
+    module_name = config['module']
+    class_name = config['name']
+    args = config['args']
+    args.update({'device': device})
+    model = create_instance(module_name, class_name, args, in_dim, out_dim)
+    return model
+
+
+def get_dataset(config, device):
+    module_name = config['module']
+    class_name = config['name']
+    args = config['args']
+    dataset = create_instance(module_name, class_name, args, device)
+    return dataset
+
+
+def get_data_loader(config, dataset):
+    module_name = config['module']
+    class_name = config['name']
+    args = config['args']
+    loader = create_instance(module_name, class_name, args, dataset)
+
+    return loader
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
