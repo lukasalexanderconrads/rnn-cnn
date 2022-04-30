17009301737f2d1c99de9670b4b3c24ef2b8b836
lukasalexanderconrads <Abba1024-github>
2022-04-30 15:05:03 +0200

    added different loss types, scheduler, and timestamps

--- src/models/simple.py
+++ src/models/simple.py
@@ -40,9 +40,9 @@ class MLP(Model):
 
         optimizer.zero_grad()
 
-        prediction = self.forward(input)
+        logits = self.forward(input)
 
-        loss = self.get_loss(prediction, target)
+        loss = self.loss(logits, target)
 
         loss.backward()
         optimizer.step()
@@ -55,13 +55,13 @@ class MLP(Model):
 
         logits = self.forward(input)
 
-        loss = self.get_loss(logits, target)
+        loss = self.loss(logits, target)
         accuracy = self.get_accuracy(logits, target)
 
         return {'loss': loss, 'accuracy': accuracy}
 
     @staticmethod
-    def get_loss(logits, target):
+    def loss(logits, target):
         return nn.functional.cross_entropy(logits, target)
 
     @staticmethod
@@ -82,6 +82,8 @@ class RNN(MLP):
             out_dim: int, number of output neurons
         """
         super(MLP, self).__init__(**kwargs)
+
+        # define network architecture
         fc_dims = kwargs.get('fc_dims')
         rnn_dim = kwargs.get('rnn_dim')
 
@@ -99,7 +101,13 @@ class RNN(MLP):
 
         self.get_logits = nn.Linear(rnn_dim, out_dim).to(self.device)
 
-    def forward(self, input, max_rec=1):
+        # get recurrency parameters
+        self.max_rec = kwargs.get('max_rec', 10)
+        self.threshold = kwargs.get('threshold', .9)
+        self.skip_connections = kwargs.get('skip_connections', False)
+        self.loss_type = kwargs.get('loss_type', 'final') # first, every, final
+
+    def forward(self, input):
         """
         :param input: tensor [batch_size, input_dim]
         :return: output: tensor [batch_size, output_dim]
@@ -108,10 +116,123 @@ class RNN(MLP):
             input = input.unsqueeze(0)
 
         h = self.fc_layers(input)
-        for _ in range(max_rec):
-            h = self.rnn_layer(h)
+        # store the step index where a logit was above the threshold for the first time
+        final_steps = torch.full((input.size(0),), self.max_rec-1)
+        logits_list = []
+        for step in range(self.max_rec):
+            # recurrent layer
+            h_new = self.rnn_layer(h)
+            h = h + h_new if self.skip_connections else h_new
+            # get class probs
+            logits = self.get_logits(h)
+            logits_list.append(logits)
 
-        return self.get_logits(h)
+            # update final_steps
+            probs = nn.functional.softmax(logits, dim=1)  # [batch_size, n_classes]
+            done_mask = torch.logical_and(torch.max(probs, dim=1).values > self.threshold,
+                                          final_steps == self.max_rec-1)
+            final_steps[done_mask] = step                   # [batch_size]
+
+            # break if all examples have finished
+            if torch.all(final_steps != self.max_rec-1):
+                break
+
+        logits_stacked = torch.stack(logits_list)           # [max_rec, batch_size, n_classes]
+
+        return logits_stacked, final_steps
+
+    def train_step(self, minibatch, optimizer: torch.optim.Optimizer):
+        input = minibatch['input']
+        target = minibatch['target']
+
+        optimizer.zero_grad()
+
+        logits_stacked, final_steps = self.forward(input)
+
+        loss = self.loss(logits_stacked, target, final_steps)
+
+        loss.backward()
+        optimizer.step()
+
+        return {'loss': loss}
+
+    def test_step(self, minibatch):
+        input = minibatch['input']
+        target = minibatch['target']
+
+        logits_stacked, final_steps = self.forward(input)
+
+        loss = self.loss(logits_stacked, target, final_steps)
+        metrics = self.metrics(logits_stacked, target, final_steps)
+
+        return {'loss': loss} | metrics
+
+    def loss(self, logits_stacked, target, final_steps):
+        """
+        computes loss
+        :param logits_stacked: logits for each example at each step [max_rec, batch_size, n_classes]
+        :param target: target classes [batch_size]
+        :param final_steps: indices corresponding to the final step for each example [batch_size]
+        :return:
+        """
+        if self.loss_type == 'first':
+            logits = logits_stacked[0]
+        elif self.loss_type == 'every':
+            logits, target = self.get_valid_logits_target(logits_stacked, target, final_steps)
+        elif self.loss_type == 'final':
+            logits = self.get_final_logits(logits_stacked, final_steps)
+
+        loss = nn.functional.cross_entropy(logits, target)
+        return loss
+
+
+    def metrics(self, logits_stacked, target, final_steps):
+        logits = self.get_final_logits(logits_stacked, final_steps)
+        accuracy = self.get_accuracy(logits, target)
+        # compute average number of recurrences required
+        avg_steps = torch.mean(final_steps.float()) + 1
+
+        ce_loss = nn.functional.cross_entropy(logits, target)
+
+        return {'cross_entropy': ce_loss, 'accuracy': accuracy, 'average steps': avg_steps}
+
+
+    def get_final_logits(self, logits_stacked, final_steps):
+        """
+        compute final logits for each example
+        :param logits_stacked: logits for each example at each step [max_rec, batch_size, n_classes]
+        :param final_steps: indices corresponding to the final step for each example [batch_size]
+        :return: logits [batch_size, n_classes]
+        """
+        # get final logits for each example
+        final_steps_exp = final_steps.view(1, -1, 1).expand(-1, -1, logits_stacked.size(-1))  # [1, batch_size, n_classes]
+        logits = torch.gather(logits_stacked,
+                              index=final_steps_exp,
+                              dim=0).squeeze()
+        return logits
+
+    def get_valid_logits_target(self, logits_stacked, target, final_steps):
+        """
+        flatten logits for steps before the final one and repeat target accordingly
+        :param logits_stacked: logits for each example at each step [max_rec, batch_size, n_classes]
+        :param target: target classes [batch_size]
+        :param final_steps: indices corresponding to the final step for each example [batch_size]
+        :return: logits [n_valid_outputs, n_classes], target [n_valid_outputs]
+        """
+        batch_size = target.size(0)
+        n_classes = logits_stacked.size(-1)
+        # mask for the valid logits
+        arange_exp = torch.arange(0, self.max_rec).unsqueeze(1).expand(-1, batch_size)  # [max_rec, batch_size]
+        final_steps_exp = final_steps.unsqueeze(0).expand(self.max_rec, -1)     # [max_rec, batch_size]
+        valid_mask = arange_exp <= final_steps_exp              # [max_rec, batch_size]
+
+        valid_indices = valid_mask.flatten().nonzero().squeeze()          # [max_rec * batch_size]
+        valid_logits = logits_stacked.view(-1, n_classes)[valid_indices]    # [n_valid_logits, n_classes]
+        valid_target = target.repeat_interleave(self.max_rec)[valid_indices]    # [n_valid_logits]
+
+        return valid_logits, valid_target
+
+
 
 
 
--- src/trainer.py
+++ src/trainer.py
@@ -1,5 +1,6 @@
 import os
 from tqdm import tqdm
+from datetime import datetime
 
 from torch.utils.tensorboard import SummaryWriter
 from src.utils import MetricAccumulator
@@ -22,15 +23,20 @@ class Trainer:
 
         self.n_epochs = kwargs.get('n_epochs')
 
+        self.loss_scheduler = kwargs.get('loss_scheduler', None)
+
         # logging
+        timestamp = self.get_timestamp()
         log_dir = kwargs.get('log_dir')
-        log_dir = os.path.join(log_dir, name)
+        log_dir = os.path.join(log_dir, name, timestamp)
         self.writer = SummaryWriter(log_dir=log_dir)
 
         self.metric_avg = MetricAccumulator()
 
     def train(self):
         for epoch in tqdm(range(self.n_epochs), desc='epochs'):
+            if self.loss_scheduler is not None:
+                self.update_loss_type(epoch)
             self.model.train()
             self.train_epoch(epoch)
 
@@ -47,6 +53,7 @@ class Trainer:
             self.metric_avg.update(metrics)
         metrics = self.metric_avg.get_average()
         self.writer.add_scalar('train/loss', metrics['loss'], epoch)
+        self.log(metrics, epoch)
 
     def test_epoch(self, epoch):
         self.metric_avg.reset()
@@ -54,9 +61,25 @@ class Trainer:
             metrics = self.model.test_step(minibatch)
             self.metric_avg.update(metrics)
         metrics = self.metric_avg.get_average()
-        self.writer.add_scalar('test/loss', metrics['loss'], epoch)
-        self.writer.add_scalar('test/accuracy', metrics['accuracy'], epoch)
+        self.log(metrics, epoch, 'test')
 
+    def update_loss_type(self, epoch):
+        epoch_frac = epoch / self.n_epochs
+        if self.loss_scheduler['first_step'] < epoch_frac:
+            self.model.loss_type = 'first'
+        elif self.loss_scheduler['every_step'] < epoch_frac:
+            self.model.loss_type = 'every'
+        else:
+            self.model.loss_type = 'final'
+
+    def log(self, metrics, epoch, split='train'):
+        for key, value in metrics.items():
+            self.writer.add_scalar(f'{split}/{key}', value, epoch)
+
+    def get_timestamp(self):
+        dt_obj = datetime.now()
+        timestamp = f'{dt_obj.month}{dt_obj.day}-{dt_obj.hour}{dt_obj.minute}{dt_obj.second}'
+        return timestamp
 
 
 
